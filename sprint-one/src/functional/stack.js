72bcbe37905c939711eeaa69e0911740148e3639
Hack Reactor Student <student@hackreactor.com>
2017-07-05 14:55:55 -0700

    Completed Stack Functional Pattern

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,20 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+  // add an element to the top of the stack (storage)
+     storage[Object.keys(storage).length] = value;
   };
 
   someInstance.pop = function() {
+    //removes the top element of the stack (storage)
+    var poppedValue = storage[Object.keys(storage).length - 1];
+    delete storage[Object.keys(storage).length - 1];
+    return poppedValue;
   };
 
   someInstance.size = function() {
+    //Returns the size of storage 
+    return Object.keys(storage).length;
   };
 
   return someInstance;
