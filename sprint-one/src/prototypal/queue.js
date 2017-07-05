5caf162e8628825920640fe9ba3acb2d9b8b648c
Hack Reactor Student <student@hackreactor.com>
2017-07-05 16:05:01 -0700

    Completed pseudoclassical stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  return someInstance;
 };
 
-var queueMethods = {};
-
-
+var queueMethods = {
+  enqueue: function(val) {
+    var keys = Object.keys(this.storage);
+    for (var i = keys.length - 1; i >= 0; i--) {
+      this.storage[i + 1] = this.storage[i];
+    }
+    this.storage[0] = val;
+    return keys.length;
+  },
+  dequeue: function() {
+    var keys = Object.keys(this.storage);
+    var popped = this.storage[keys.length - 1];
+    delete this.storage[keys.length - 1];
+    return popped;
+  },
+  size: function() {
+    var keys = Object.keys(this.storage);
+    return keys.length;
+  }
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(val) {
+  var keys = Object.keys(this.storage);
+  this.storage[keys.length] = val;
+  return keys.length;
+};
+
+stackMethods.pop = function() {
+  var keys = Object.keys(this.storage);
+  var popped = this.storage[keys.length - 1];
+  delete this.storage[keys.length - 1];
+  return popped;
+};
+
+stackMethods.size = function() {
+  var keys = Object.keys(this.storage);
+  return keys.length;
+};
 
