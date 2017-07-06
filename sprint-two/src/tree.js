975c1a9b6a0ed9bf92272ebe8d02e3eaf7788bae
Hack Reactor Student <student@hackreactor.com>
2017-07-06 16:13:10 -0700

    Completed Tree Data Structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,20 +3,43 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var node = Tree(value);
+  this.children.push(node);
 };
 
 treeMethods.contains = function(target) {
+  var doesContainValue = false;
+  if (this.value === target) {
+    doesContainValue = true;
+    return doesContainValue;
+  }
+  
+  var transverse = function(parent) {
+    if (parent.children.length !== 0 ) {
+      parent.children.forEach(function(child) {
+        if (child.value === target) {
+          doesContainValue = true;
+          return;
+        } else {
+          transverse(child);
+        }
+      });  
+    }    
+  };
+  
+  transverse(this);
+  return doesContainValue;
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
