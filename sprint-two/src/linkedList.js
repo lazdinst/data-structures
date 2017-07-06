2f80399118390b56e7533cfe1221c3ac76bfbd3b
Hack Reactor Student <student@hackreactor.com>
2017-07-06 11:37:38 -0700

    implements Link List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,53 @@
 var LinkedList = function() {
   var list = {};
   list.head = null;
-  list.tail = null;
+  list.tail = null; 
+  
 
   list.addToTail = function(value) {
+    // list.tail = value;
+    if (list.head === null) {
+      list.head = new Node(value);
+      list.tail = list.head;
+    } else if (list.head === list.tail) {
+      var node = new Node(value);
+      list.tail = node;
+      list.head.next = list.tail;
+    } else {
+      // var node = new Node(value);
+      // list.tail = node;
+      //Do Something
+    }
+    
+
+    // node.next = null;
   };
 
   list.removeHead = function() {
+    var newHead = list.head.next;
+    var formerHead = list.head.value;
+    if (list.head.next === list.tail) {
+      list.head = newHead;      
+      
+      // list.tail = list.head;
+    } else {
+      // list.head.next = list.head.next.next;  
+      // list.head = newHead;
+    }
+    return formerHead;
   };
 
   list.contains = function(target) {
+    var nextNode = list.head;
+ 
+    while (nextNode !== null) {
+      if (nextNode.value === target) {
+        return true;
+      } 
+      nextNode = nextNode.next;
+    }
+
+    return false;
   };
 
   return list;
@@ -27,3 +65,4 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
