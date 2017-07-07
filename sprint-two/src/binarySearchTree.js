62f38eaa69f06a670196c329469d1b92b1545cdf
Hack Reactor Student <student@hackreactor.com>
2017-07-07 16:40:18 -0700

    Commiting because computer is acting up

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,53 @@
 var BinarySearchTree = function(value) {
+  var tree = {};
+  
+  tree.root = value;
+  tree.left = null;
+  tree.right = null; 
+  tree.insert = function(value) {
+  //If val < root
+    if (this.root > value) {
+      // if this.left !== null/
+      if (this.left === null) {
+        //add node to this.left
+        this.left = BinarySearchTree(value);
+      } else {
+        //enter recursion
+        traverseTree(this.left);
+      }
+    } else if (this.root < value) {
+        // if this.right !== Null
+        if (this.right === null) {
+          //add node to this.right
+          this.right = BinarySearchTree(value);
+        } else {
+          //enter recursion
+          traverseTree(this.right);
+        }
+    }
+  
+    var traverseTree = function(value) {
+      if (value < this.root) { 
+        this.left = BinarySearchTree(value);
+      } else if (value > this.root) { 
+        this.right = BinarySearchTree(value);
+      }
+      return;
+    };
+  };
+    
+  tree.contains = function(value) {
+    
+    
+    //returns boolean
+  };
+
+  tree.depthFirstLog = function(cb) {
+    //Will call callback on each node in the tree
+    
+  };
+
+  return tree;
 };
 
 
