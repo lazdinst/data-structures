c979599ba5d24b9f6e5ae293b272c9aacb7deeb0
Hack Reactor Student <student@hackreactor.com>
2017-07-08 09:35:49 -0700

    Added additional test case for LinkedList.js

--- sprint-two/spec/linkedListSpec.js
+++ sprint-two/spec/linkedListSpec.js
@@ -51,5 +51,12 @@ describe('linkedList', function() {
     expect(linkedList.contains(4)).to.equal(false);
   });
 
+  it('should return null if linked list is empty', function() {
+    linkedList.addToTail(4);
+    linkedList.removeHead();
+    expect(linkedList.head).to.equal(null);
+    expect(linkedList.tail).to.equal(null);
+  });
+
   // add more tests here to test the functionality of linkedList
 });
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,7 +3,6 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null; 
   
-
   list.addToTail = function(value) {
     // list.tail = value;
     if (list.head === null) {
@@ -14,54 +13,45 @@ var LinkedList = function() {
       list.tail = node;
       list.head.next = list.tail;
     } else {
-      // var node = new Node(value);
-      // list.tail = node;
       //Do Something
     }
-
-    // node.next = null;
   };
 
   list.removeHead = function() {
     var newHead = list.head.next;
     var formerHead = list.head.value;
+    if (list.head.value === list.tail.value) {
+      list.head = null;
+      list.tail = null;
+      return formerHead;
+    }
+
     if (list.head.next === list.tail) {
-      list.head = newHead;      
-      
-      // list.tail = list.head;
-    } else {
-      // list.head.next = list.head.next.next;  
-      // list.head = newHead;
+      list.head = newHead;
     }
     return formerHead;
   };
 
   list.contains = function(target) {
     var nextNode = list.head;
- 
     while (nextNode !== null) {
       if (nextNode.value === target) {
         return true;
       } 
       nextNode = nextNode.next;
     }
-
     return false;
   };
-
   return list;
 };
 
 var Node = function(value) {
   var node = {};
-
   node.value = value;
   node.next = null;
-
   return node;
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
