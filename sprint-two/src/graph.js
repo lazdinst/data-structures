d09f4fdb6267e9ecdb0f00489a03bf574028ec40
Hack Reactor Student <student@hackreactor.com>
2017-07-07 21:56:14 -0700

    Completed Graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,11 +3,8 @@
 // Instantiate a new graph
 var Graph = function() {
   this.nodes = [];
-
+  this.edges = [];
   // [ {id: 2, edges: [3  ]},  {id: 3, edges: [ 2 ]}, {}]
-
-  // Adjacency List ex [1,2]
-  // Adjacency Matrix [  ]
 };
 
 // Add a node to the graph, passing in the node's value.
@@ -27,68 +24,119 @@ Graph.prototype.contains = function(node) {
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
   
+  
+  this.nodes = this.nodes.filter(function(currentNode) {
+    return currentNode.id !== node;
+  });
+  
+  if (this.edges.length) {
+    var edgesToRemove = this.edges.filter(function(edge) {
+      return edge[0] === node;
+    });
+    Graph.prototype.removeEdge.call(this, edgesToRemove[0][0], edgesToRemove[0][1]);
+  }
+  
+  
   // nodesEdges.forEach(function(currentEdge) {
   //   //[2,3]
   //   removeEdge(currentEdge, node);
   // });
-  var nodeEdges = this.nodes.find(function(currentNode) {
-    return currentNode.id === node;
-  }).edges;
 
-  if (nodeEdges.length) {
-    for (var i = 0; i < nodeEdges.length; i++) {
-      currentEdgeToRemove = nodeEdges[i];
-      Graph.prototype.removeEdge.apply(this, [currentEdgeToRemove, node]);
-    }
-  }
+// debugger;
 
-  this.nodes = this.nodes.filter(function(currentNode) {
-    return currentNode.id !== node;
-  });
+//   var nodeEdges = this.nodes.find(function(currentNode) {
+//     return currentNode.id === node;
+//   }).edges;
+
+//   if (nodeEdges.length) {
+//     for (var i = 0; i < nodeEdges.length; i++) {
+//       currentEdgeToRemove = nodeEdges[i];
+//       Graph.prototype.removeEdge.apply(this, [currentEdgeToRemove, node]);
+//     }
+//   }
+  
+
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  //  if(currentNode.edges.includes(fromNode) && )
-  var hasToNode = this.nodes.filter(function(currentNode) {
-    return currentNode.id === fromNode; 
-  })[0].edges.includes(toNode);
 
-  var hasFromNode = this.nodes.filter(function(currentNode) {
-    return currentNode.id === toNode; 
-  })[0].edges.includes(fromNode);
+  var hasFromNode = this.edges.filter(function(edgeArray) {
+    return edgeArray[0] === fromNode;
+  });
+
+  var hasToNode = this.edges.filter(function(edgeArray) {
+    return edgeArray[0] === toNode;
+  });
+
+  return hasToNode.length && hasFromNode.length ? true : false;
   
-  return hasToNode && hasFromNode;
+//======================= OLD CODE =================================
+  // return result === undefined ? false : true;
+
+  //  if(currentNode.edges.includes(fromNode) && )
+  
+  // var hasToNode = this.nodes.filter(function(currentNode) {
+  //   return currentNode.id === fromNode; 
+  // })[0].edges.includes(toNode);
+
+  // var hasFromNode = this.nodes.filter(function(currentNode) {
+  //   return currentNode.id === toNode; 
+  // })[0].edges.includes(fromNode);
+  
+  // return hasToNode && hasFromNode;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  this.nodes.forEach(function(currentNode) {
-    if (currentNode.id === fromNode) {
-      currentNode.edges.push(toNode);
-    }
-    if (currentNode.id === toNode) {
-      currentNode.edges.push(fromNode);
-    }
-  });
+  this.edges.push([fromNode, toNode]);
+  this.edges.push([toNode, fromNode]);
+
+//======================= OLD CODE =================================
+  // this.nodes.forEach(function(currentNode) {
+  //   if (currentNode.id === fromNode) {
+  //     currentNode.edges.push(toNode);
+  //   }
+  //   if (currentNode.id === toNode) {
+  //     currentNode.edges.push(fromNode);
+  //   }
+  // });
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  this.nodes.forEach(function(currentNode) {
-    if (currentNode.id === fromNode) {
-      var toNodeIndex = currentNode.edges.indexOf(toNode);
-      currentNode.edges.splice(toNodeIndex, 1);
-    }
-    if (currentNode.id === toNode) {
-      var fromNodeIndex = currentNode.edges.indexOf(fromNode);
-      currentNode.edges.splice(fromNodeIndex, 1);
-    }
+  // create array [fromNode, toNode]
+  var forwardEdge = [fromNode, toNode];
+  var reverseEdge = [toNode, fromNode];
+  // filter on the this.edges
+  var result = this.edges.filter(function(edge) {
+    return JSON.stringify(edge) !== JSON.stringify(forwardEdge) && JSON.stringify(edge) !== JSON.stringify(reverseEdge);
   });
+  this.edges = result;
+  // filter out all edges that have both forward or backward
+  // set new this.edges to filtered arrray
+
+//======================= OLD CODE =================================
+  // this.nodes.forEach(function(currentNode) {
+  //   if (currentNode.id === fromNode) {
+  //     var toNodeIndex = currentNode.edges.indexOf(toNode);
+  //     currentNode.edges.splice(toNodeIndex, 1);
+  //   }
+  //   if (currentNode.id === toNode) {
+  //     var fromNodeIndex = currentNode.edges.indexOf(fromNode);
+  //     currentNode.edges.splice(fromNodeIndex, 1);
+  //   }
+  // });
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  debugger;
+
+  this.nodes.forEach(function(node) {
+    cb(node.id);
+  });
+  
 };
 
 /*
